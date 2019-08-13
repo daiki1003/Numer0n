46a371a3c6cef6023c7a9db7d1fae640b9d8f797
Daiki Asahi <dalto.saxphone@gmail.com>
2019-08-13 10:12:47 +0900

    answer: 桁重複を許さない正解を生成出来る様に実装。

--- Numer0n/Answer.swift
+++ Numer0n/Answer.swift
@@ -9,7 +9,31 @@
 import Foundation
 
 class Answer {
-    
+
     static var digits: Int = 4
-    
+
+    var numbers: Array<Int> = []
+
+    init() {
+        self.generateNumbers()
+    }
+
+    // 正解の生成
+    private func generateNumbers() {
+        for _ in 0 ..< Answer.digits {
+            self.numbers.append(self.generateNumber())
+        }
+    }
+
+    // 一桁分の正解の生成
+    private func generateNumber() -> Int {
+
+        let numberCandidate: Int = Int.random(in: 0 ..< 10)
+        for number in self.numbers {
+            if number == numberCandidate { return self.generateNumber() }
+        }
+
+        return numberCandidate
+
+    }
 }
